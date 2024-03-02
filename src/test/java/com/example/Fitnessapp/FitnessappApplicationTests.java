7946363c7d80c3887f270fe3339f95120e2e69ea
Phillip <phillipm@udel.edu>
2024-03-02 14:23:40 -0500

    Initializing project

---
+++ src/test/java/com/example/Fitnessapp/FitnessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
